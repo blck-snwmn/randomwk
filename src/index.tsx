4747287eea34b8550e8d8425c9b4866f71d71d2a
blck-snwmn <whitesnowmancreator@gmail.com>
2025-01-11 05:03:12 +0900

    Add YoutubeVideoSnippet type

--- src/index.tsx
+++ src/index.tsx
@@ -107,25 +107,27 @@ interface YoutubeVideo {
 	id: {
 		videoId: string;
 	};
-	snippet: {
-		title: string;
-		description: string;
-		thumbnails: {
-			default: {
-				url: string;
-				width: number;
-				height: number;
-			};
-			medium: {
-				url: string;
-				width: number;
-				height: number;
-			};
-			high: {
-				url: string;
-				width: number;
-				height: number;
-			};
+	snippet: YoutubeVideoSnippet;
+}
+
+interface YoutubeVideoSnippet {
+	title: string;
+	description: string;
+	thumbnails: {
+		default: {
+			url: string;
+			width: number;
+			height: number;
+		};
+		medium: {
+			url: string;
+			width: number;
+			height: number;
+		};
+		high: {
+			url: string;
+			width: number;
+			height: number;
 		};
 	};
 }
